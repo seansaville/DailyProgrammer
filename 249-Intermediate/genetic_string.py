5e8e427c432fa1156c0e853a1b96156d81daa7ee
Sean Saville <sean.saville@hotmail.co.uk>
2016-09-24 16:57:38 +0100

    #249 Intermediate - Code now conforms to PEP-8

--- 249-Intermediate/genetic_string.py
+++ 249-Intermediate/genetic_string.py
@@ -2,13 +2,15 @@ import random
 import string
 import sys
 
-# breed_strings
-#
-# Takes two strings and breeds them together to produce a new child string. Each
-# character in the child string has an equal chance of coming from either of the
-# parents.
-#
+
 def breed_strings(string1, string2):
+    """
+    Takes two strings and breeds them together to produce a new child string.
+
+    Each character in the child string has an equal chance of coming from
+    either of the parents.
+
+    """
     child = ""
 
     for c1, c2 in zip(string1, string2):
@@ -19,12 +21,14 @@ def breed_strings(string1, string2):
 
     return child
 
-# calculate_fitness
-#
-# Takes a population (list of candidate strings) and calculates the fitness of
-# each, returning a dictionary mapping candidate strings to their fitness.
-#
+
 def calculate_fitness(pop, target):
+    """
+    Takes a population (list of candidate strings) and calculates the fitness
+    of each, returning a dictionary mapping candidate strings to their
+    fitness.
+
+    """
     fitdict = {}
 
     for str in pop:
@@ -32,26 +36,27 @@ def calculate_fitness(pop, target):
 
     return fitdict
 
-# generate_initial_pop
-#
-# Generates an initial population of random strings of the specified length.
-#
+
 def generate_initial_pop(popsize, length):
+    """
+    Generates an initial population of random strings of the specified length.
+    """
     charset = string.ascii_letters + string.digits + string.punctuation + " "
     pop = []
 
     for i in range(0, popsize):
         rand = ''.join([random.choice(charset) for n in range(length)])
         pop.append(rand)
-    
+
     return pop
 
-# hamming_distance
-#
-# Returns the Hamming distance between two strings, i.e. the number of positions 
-# with differing characters.
-#
+
 def hamming_distance(string1, string2):
+    """
+    Returns the Hamming distance between two strings, i.e. the number of
+    positions with differing characters.
+
+    """
     distance = 0
 
     for c1, c2 in zip(string1, string2):
@@ -60,11 +65,12 @@ def hamming_distance(string1, string2):
 
     return distance
 
-# mutate
-#
-# Randomly mutates a given string with a probability.
-#
+
 def mutate(str, prob):
+    """
+    Randomly mutates a given string with a probability.
+
+    """
     out = ""
     charset = string.ascii_letters + string.digits + string.punctuation + " "
 
@@ -80,12 +86,13 @@ def mutate(str, prob):
 
     return out
 
-# sel_breeding_pair
-#
-# From a dictionary mapping candidate strings to their fitness, select a pair of
-# strings to breed together using fitness-proportionate selection.
-#
+
 def sel_breeding_pair(fitdict):
+    """
+    Uses fitness-proportionate selection to a select a breeding pair of
+    strings from a dictionary mapping candidate strings to their fitness.
+
+    """
     total_fitness = 0
     for val in fitdict.values():
         total_fitness += val
@@ -94,7 +101,7 @@ def sel_breeding_pair(fitdict):
     for str in fitdict:
         probs[str] = fitdict[str] / total_fitness
 
-    candidates = ["",""]
+    candidates = ["", ""]
     for i in range(0, 2):
         chosen = random.random()
         for str in probs:
@@ -105,14 +112,15 @@ def sel_breeding_pair(fitdict):
 
     return candidates
 
-# genetic_string_gen
-#
-# Given a target string, use a genetic algorithm to produce that string starting
-# from an initial random population of strings.
-#
+
 def genetic_string_gen(target, popsize, mutchance):
+    """
+    Uses a genetic algorithm to produce a target string, starting from an
+    initial random population of strings.
+
+    """
     pop = generate_initial_pop(popsize, len(target))
-    
+
     gen = 0
     done = False
     while not done:
@@ -132,15 +140,15 @@ def genetic_string_gen(target, popsize, mutchance):
             nextgen.append(child)
 
         if fittestcandidate == target:
-            done = True;
+            done = True
 
         print("Generation {} | {} | Score: {}"
-                .format(gen, fittestcandidate, fittestcandidatescore))
+              .format(gen, fittestcandidate, fittestcandidatescore))
 
         pop = nextgen
         gen += 1
 
-# main
+
 def main():
     try:
         target = sys.argv[1]
@@ -149,7 +157,7 @@ def main():
         genetic_string_gen(target, popsize, mutchance)
     except:
         print("Usage: python3 genetic_string.py <target string> "
-            "<population size> <mutation chance [0 - 1]>")
+              "<population size> <mutation chance [0 - 1]>")
 
 if __name__ == '__main__':
     main()
